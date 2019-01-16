0c2dfe09886440dd8afc8ccc74a8d091971cc140
Kasandra Wolf <wolf.kasandra@gmail.com>
2019-01-16 13:12:02 -0700

    set configs

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -13,15 +13,12 @@ namespace BurgerShack.Models
     [Range(5, float.MaxValue)]
     public float Price { get; set; }
     public int Id { get; private set; }
-    private int count = 1;
 
     public Burger(string name, string desc, float price)
     {
       Name = name;
       Description = desc;
       Price = price;
-      Id = count;
-      count++;
     }
   }
 }
